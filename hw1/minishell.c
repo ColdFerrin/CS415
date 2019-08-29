32bf30185b195038a64545982c38f66b6af499d6
coldferrin <ferrinkatz@ferrinkatz.com>
2019-08-29 16:37:55 -0700

    Add password creation logic

--- hw1/minishell.c
+++ hw1/minishell.c
@@ -12,143 +12,166 @@
  * replaced with recommended fgets with line-feed stripping.
  *
  */
-#include	<stdio.h>
-#include	<stdlib.h>
-#include	<sys/types.h>
-#include	<sys/wait.h>
-#include	<string.h>
-#include	<unistd.h>
-#include    <sys/stat.h>
-#include    <errno.h>
-#include    <time.h>
-#include    <crypt.h>
-#include	"minishell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <time.h>
+#include <crypt.h>
+#include "minishell.h"
 
 
 char *lookupPath(char **, char **);
+
 int parseCommand(char *, struct command_t *);
+
 int parsePath(char **);
+
 void printPrompt();
+
 void readCommand(char *);
+
 char *createPassword(int);
+
 void addUser(USER **, FILE *);
+
+int getUsers();
+
+int checkPasswd();
+
+int promptUsername();
 
 char promptString[] = "in a Nutt shell >";
 extern FILE *stdin;
-char* path;
-char* home;
 
-int main()
-{
+int main() {
     char commandLine[LINE_LEN];
     char *pathv[MAX_PATHS];
     int numPaths;
     int i, j, len;
-    int chPID;		// Child PID
-    int stat;		// Used by parent wait
+    int chPID;        // Child PID
+    int stat;        // Used by parent wait
     pid_t thisChPID;
-    int isLoggedIn = 0;
     int status;
     struct command_t command;
-    USER *users[10];
 
-    path = (char *) getenv("PATH");
-    home = (char *) getenv("HOME");
+    USER *users[10];
+    char *home = (char *) getenv("HOME");
+    int isLoggedIn = 0;
 
     // Shell initialization
-    for(i=0; i<MAX_ARGS; i++)
+    for (i = 0; i < MAX_ARGS; i++)
         command.argv[i] = (char *) malloc(MAX_ARG_LEN);
 
     parsePath(pathv);
 
-    char filepath[50];
+    char filepath[64];
     strcpy(filepath, home);
     strcat(filepath, "/.minishell");
 
-    FILE *fptrRead;
-    FILE *fptrAppend;
+    FILE *fptr = NULL;
 
-    if( access( filepath, F_OK ) != -1 ) {
-        fptrRead = fopen(filepath,"r");
-        fptrAppend = fopen(filepath, "a");
-    } else {
-        fptrAppend = fopen(filepath, "a");
-        addUser(users, fptrAppend);
+
+    // if file of users exists read filed load users and check if more users can be added.
+    if (access(filepath, R_OK | W_OK) != -1) {
+        FILE *fptrRead = fopen(filepath, "r");
+        int addUsers = getUsers();
+        fclose(fptrRead);
+        if (addUsers) {
+            fptr = fopen(filepath, "a");
+        }
+    } else {        //else create the users file and load for reading
+        fptr = fopen(filepath, "a");
+        addUser(users, fptr);
+    }
+
+    int userNum = promptUsername();
+
+    if (userNum == -1) {
+        printf("Username is not valid \n\n\tEXITING...\n");
+        return 127;
     }
 
     // Login Loop
-    for (int i = 0; i < 3 && !isLoggedIn; ++i) {
+    int loginAttempts;
+    for (loginAttempts = 0; loginAttempts < 3 && !isLoggedIn; ++loginAttempts) {
+        isLoggedIn = checkPasswd();
+    }
 
+    if (!isLoggedIn) {
+        printf("Attempts exceeded \n\n\tExiting...\n");
+        return 1;
     }
 
     // Main loop
-    if(isLoggedIn) {
-        while (TRUE) {
-            printPrompt();
+    while (isLoggedIn) {
+        printPrompt();
 
-            // Read the command line and parse it
-            readCommand(commandLine);
-            printf("commandLine=%s\n", commandLine);
+        // Read the command line and parse it
+        readCommand(commandLine);
+        printf("commandLine=%s\n", commandLine);
 
-            if ((strcmp(commandLine, "exit") == 0) ||
-                (strcmp(commandLine, "quit") == 0)) {
-                break;    // Quit the shell
-            }
+        if ((strcmp(commandLine, "exit") == 0) ||
+            (strcmp(commandLine, "quit") == 0)) {
+            isLoggedIn = 0;
+            break;    // Quit the shell
+        }
 
-            parseCommand(commandLine, &command);
-
-#ifdef DEBUG
-            printf("... returned from parseCommmand ...\n");
-
-            for(i=0; i<command.argc; i++)
-                printf("	argv[%d] = %s\n", i,  command.argv[i]);
-#endif
-
-            // Get the full pathname for the file
-            command.name = lookupPath(command.argv, pathv);
+        parseCommand(commandLine, &command);
 
 #ifdef DEBUG
-            printf("... returned from lookupPath ...\n");
-            printf("	command path = %s\n", command.name);
-            for(i=0; i<command.argc; i++)
+        printf("... returned from parseCommmand ...\n");
+
+        for(i=0; i<command.argc; i++)
             printf("	argv[%d] = %s\n", i,  command.argv[i]);
 #endif
 
-            if (command.name == NULL) {
-                fprintf(stderr, "Command %s not found\n", command.argv[0]);
-                continue;
-            }
-
-            // Create a process to execute the command
-            if ((chPID = fork()) == 0) {
-                //  This is the child, that will execute the command requested
+        // Get the full pathname for the file
+        command.name = lookupPath(command.argv, pathv);
 
 #ifdef DEBUG
-                printf("child executing: %s\n", command.name);
-                for(i=1; i<command.argc; i++)
-                    printf("	argv[%d] = %s\n", i,  command.argv[i]);
+        printf("... returned from lookupPath ...\n");
+        printf("	command path = %s\n", command.name);
+        for(i=0; i<command.argc; i++)
+        printf("	argv[%d] = %s\n", i,  command.argv[i]);
 #endif
 
-                execv(command.name, command.argv);
-            }
-
-            // Wait for the child to terminate
-
-#ifdef DEBUG
-            printf("Parent waiting\n");
-#endif
-
-            thisChPID = wait(&stat);
+        if (command.name == NULL) {
+            fprintf(stderr, "Command %s not found\n", command.argv[0]);
+            continue;
         }
-    }
 
+        // Create a process to execute the command
+        if ((chPID = fork()) == 0) {
+            //  This is the child, that will execute the command requested
+
+#ifdef DEBUG
+            printf("child executing: %s\n", command.name);
+            for(i=1; i<command.argc; i++)
+                printf("	argv[%d] = %s\n", i,  command.argv[i]);
+#endif
+
+            execv(command.name, command.argv);
+        }
+
+        // Wait for the child to terminate
+
+#ifdef DEBUG
+        printf("Parent waiting\n");
+#endif
+
+        thisChPID = wait(&stat);
+    }
 // Shell termination
 
 }
 
 
-char *lookupPath(char **argv, char **dir)
-{
+char *lookupPath(char **argv, char **dir) {
 
     // This function inspired by one written by Sam Siewert in Spring 1996
     int i;
@@ -156,18 +179,16 @@ char *lookupPath(char **argv, char **dir)
     char pName[MAX_PATH_LEN];
 
     // Check to see if file name is already an absolute path name
-    if(*argv[0] == '/')
-    {
-        result = (char *) malloc(strlen(argv[0])+1);
+    if (*argv[0] == '/') {
+        result = (char *) malloc(strlen(argv[0]) + 1);
         strcpy(result, argv[0]);
         return result;
     }
 
     // Look in PATH directories
     // This code does not handle the case of a relative pathname
-    for(i = 0; i < MAX_PATHS; i++)
-    {
-        if(dir[i] == NULL) break;
+    for (i = 0; i < MAX_PATHS; i++) {
+        if (dir[i] == NULL) break;
         strcpy(pName, dir[i]);
         strcat(pName, "/");
         strcat(pName, argv[0]);
@@ -176,17 +197,16 @@ char *lookupPath(char **argv, char **dir)
         printf("lookupPath: Checking for %s\n", pName);
 #endif
 
-        if(access(pName, X_OK | F_OK) != -1)
-        {
+        if (access(pName, X_OK | F_OK) != -1) {
 
             // File found
 #ifdef DEBUG
             printf("lookupPath: Found %s in %s (full path is %s)\n",
-			argv[0], dir[i], pName);
+            argv[0], dir[i], pName);
 #endif
-            result = (char *) malloc(strlen(pName)+1);
+            result = (char *) malloc(strlen(pName) + 1);
             strcpy(result, pName);
-            return result;		// Return with success
+            return result;        // Return with success
         }
     }
 
@@ -197,8 +217,7 @@ char *lookupPath(char **argv, char **dir)
 }
 
 
-int parseCommand(char *cLine, struct command_t *cmd)
-{
+int parseCommand(char *cLine, struct command_t *cmd) {
     // Determine command name and construct the parameter list
     int argc;
     int i, j;
@@ -210,40 +229,37 @@ int parseCommand(char *cLine, struct command_t *cmd)
 
     // Get the command name and parameters
     // This code does not handle multiple WHITESPACE characters
-    while((cmd->argv[argc++] = strsep(clPtr, WHITESPACE)) != NULL) ;
+    while ((cmd->argv[argc++] = strsep(clPtr, WHITESPACE)) != NULL);
 
-    cmd->argv[argc--] = '\0';	// Null terminated list of strings
+    cmd->argv[argc--] = '\0';    // Null terminated list of strings
     cmd->argc = argc;
 
-    return  1;
+    return 1;
 }
 
 
-int parsePath(char *dirs[])
-{
+int parsePath(char *dirs[]) {
     // This routine based on one written by Panos Tsirigotis, Spring 1989
     int i;
     char *pathEnvVar;
     register char *thePath, *oldp;
 
-    for(i=0; i<MAX_ARGS; i++)
+    for (i = 0; i < MAX_ARGS; i++)
         dirs[i] = NULL;
 
-    pathEnvVar = path;
+    pathEnvVar = (char *) getenv("PATH");
     thePath = (char *) malloc(strlen(pathEnvVar) + 1);
     strcpy(thePath, pathEnvVar);
 
     i = 0;
     oldp = thePath;
 
-    for(;; thePath++)
-    {
-        if((*thePath == ':') || (*thePath == '\0'))
-        {
+    for (;; thePath++) {
+        if ((*thePath == ':') || (*thePath == '\0')) {
             dirs[i] = oldp;
             i++;
 
-            if(*thePath == '\0') break;
+            if (*thePath == '\0') break;
 
             *thePath = '\0';
             oldp = thePath + 1;
@@ -254,19 +270,17 @@ int parsePath(char *dirs[])
     printf("Directories in PATH variable\n");
         for(i=0; i<MAX_PATHS; i++)
             if(dirs[i] != '\0')
-		printf("	Directory[%d]: %s\n", i, dirs[i]);
+        printf("	Directory[%d]: %s\n", i, dirs[i]);
 #endif
 
 }
 
 
-void printPrompt()
-{
+void printPrompt() {
     printf("%s ", promptString);
 }
 
-void readCommand(char *buffer)
-{
+void readCommand(char *buffer) {
     // original dangerous stdio call - can over-run buffer, see man gets
     // gets(buffer); 
 
@@ -276,26 +290,25 @@ void readCommand(char *buffer)
     // 
     fgets(buffer, LINE_LEN, stdin);
 
-    buffer[strlen(buffer)-1] = '\0';  // overwrite the line feed with null term
+    buffer[strlen(buffer) - 1] = '\0';  // overwrite the line feed with null term
 
     printf("readCommand buffer=%s\n", buffer);
 }
 
 
-char *createPassword(int allowRetry){
+char *createPassword(int allowRetry) {
     char salt[] = "$6$................";
-    const char *const saltChars = 
-        "./0123456789ABCDEFGHIJKLMNOPQRST"
-        "UVWXYZabcdefghijklmnopqrstuvwxyz";
+    const char *const saltChars =
+            "./0123456789ABCDEFGHIJKLMNOPQRST"
+            "UVWXYZabcdefghijklmnopqrstuvwxyz";
 
     char *password;
-    char *attempt1;
     char *attemptStatus;
 
     srandom(time(NULL));
 
-    for(int i = 0; i < 16; i++){
-        salt[i+3] = saltChars[random() & 64];
+    for (int i = 0; i < 16; ++i) {
+        salt[i + 3] = saltChars[random() % 64];
     }
 
     int passwordsMatch = 0;
@@ -304,27 +317,37 @@ char *createPassword(int allowRetry){
         do {
             char *passAttempt = getpass("New password: ");
 
-            if(strlen(passAttempt) < 8) {
+            if (strlen(passAttempt) < 8) {
                 attemptStatus = "BAD PASSWORD: Must be at least 8";
-            }
-            else if(strlen(passAttempt) > 20) {
+            } else if (strlen(passAttempt) > 20) {
                 attemptStatus = "BAD PASSWORD: Must be at most 20";
-            }
-            else {
-                attempt1 = crypt(passAttempt, salt);
+            } else {
+                password = crypt(passAttempt, salt);
                 goodAttempt = 1;
+            }
+
+            if(!goodAttempt){
+                printf("%s\n", attemptStatus);
             }
         } while (!goodAttempt);
 
         char *passMatch = crypt(getpass("Retype new password: "), salt);
 
-        if (strcmp())
+        if (strcmp(password, passMatch) == 0){
+            passwordsMatch = 1;
+        }
+
+        if(!passwordsMatch && !allowRetry){
+            printf("Passwords do not match\n");
+            return NULL;
+        }
     } while (!passwordsMatch && allowRetry);
 
+    return password;
 }
 
 
-void addUser(USER ** users, FILE *fptrAppend){
+void addUser(USER **users, FILE *fptrAppend) {
     USER *existingUsers = *users;
 
     USER *newUser = malloc(sizeof(USER));
@@ -333,5 +356,13 @@ void addUser(USER ** users, FILE *fptrAppend){
     char password[MAX_PASSWD + 1];
 
 
-
 }
+
+
+int getUsers();
+
+
+int checkPasswd();
+
+
+int promptUsername();
